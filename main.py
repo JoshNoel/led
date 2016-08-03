ed40f264573188eefb189ad208bf7e1b6f9b4649
Josh Noel <joshnoel@mit.edu>
2016-08-02 20:43:21 -0400

    Now writes to GPIO

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 
 def main():
     print(GPIO.RPI_INFO['PI_REVISION'])
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setwarnings(False)
-    #GPIO.setup(17, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(17, GPIO.OUT, initial=GPIO.HIGH)
 
     GPIO.cleanup()
 
