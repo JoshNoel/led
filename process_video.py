c66e1987d5ae3701fdcc97a783c8ea21849a1798
Josh Noel <joshnoel@mit.edu>
2016-08-14 00:45:29 -0400

    Videos are now processed using an input fps to calculate control times, so that the times are not affected by performance of the script. clists updated to match change

--- process_video.py
+++ process_video.py
@@ -27,7 +27,10 @@ def setGreen(b, control_list, time):
         green_on = b
         control_list.append("Red-" + str(time))
 
-def proc_vid(vid_path):
+def calc_time(frame_num, ifps):
+    return ifps * frame_num
+
+def proc_vid(vid_path, ifps, show_frames):
     global red_on
     global lower_contour_bound
     global upper_contour_bound
@@ -40,15 +43,12 @@ def proc_vid(vid_path):
         return
 
     frame_cnt = 0
-    start_time = time.clock()
     while(vid.isOpened()):
         ret, frame = vid.read()
         frame_cnt += 1
         if ret is False:
             break
         color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # print frame number on frame
 
         # extract red led
         lower_red = np.array([0, 0, 255]) # 0, 0, 100
@@ -67,20 +67,23 @@ def proc_vid(vid_path):
             if cv2.contourArea(c) > area_red:
                 area_red = cv2.contourArea(contours_red[0])
         if area_red > upper_contour_bound:
-            setRed(True, control_list, time.clock())
+            t = calc_time(frame_cnt, ifps)
+            setRed(True, control_list, t)
         elif (area_red < lower_contour_bound or len(contours_red) == 0) and red_on:
-            setRed(False, control_list, time.clock())
+            t = calc_time(frame_cnt, ifps)
+            setRed(False, control_list, t)
 
 
         # apply mask to image
         res = cv2.bitwise_and(frame, frame, mask=mask_red)
 
-        cv2.putText(res, str(frame_cnt), (10,25), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
-        cv2.namedWindow("Frame", cv2.WINDOW_NORMAL), cv2.imshow("Frame", frame), cv2.resizeWindow("Frame", 800, 800)
-        cv2.namedWindow("Mask", cv2.WINDOW_NORMAL), cv2.imshow("Mask", mask_red), cv2.resizeWindow("Mask", 800, 800)
-        cv2.namedWindow("Res", cv2.WINDOW_NORMAL), cv2.imshow("Res", res), cv2.resizeWindow("Res", 800, 800)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if show_frames:
+            cv2.putText(res, str(frame_cnt), (10,25), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+            cv2.namedWindow("Frame", cv2.WINDOW_NORMAL), cv2.imshow("Frame", frame), cv2.resizeWindow("Frame", 800, 800)
+            cv2.namedWindow("Mask", cv2.WINDOW_NORMAL), cv2.imshow("Mask", mask_red), cv2.resizeWindow("Mask", 800, 800)
+            cv2.namedWindow("Res", cv2.WINDOW_NORMAL), cv2.imshow("Res", res), cv2.resizeWindow("Res", 800, 800)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     vid.release()
     cv2.destroyAllWindows()
@@ -88,10 +91,16 @@ def proc_vid(vid_path):
     return control_list
 
 
+# args: video_path, fps, clist_output_name
 if __name__ == '__main__':
-    file_name = "./control_lists/" + sys.argv[2] + ".clist"
+    file_name = "./control_lists/" + sys.argv[3] + ".clist"
     vid_path = sys.argv[1]
-    control_list = proc_vid(vid_path)
+    fps = int(sys.argv[2])
+    b = sys.argv[4]
+    show_frames = True
+    if b.lower() == "false":
+        show_frames = False
+    control_list = proc_vid(vid_path, 1/fps, show_frames)
     file = open(file_name, 'w+')
     if len(control_list) > 0:
         file.write(" : ".join(control_list))
