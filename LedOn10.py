9f4815db7f600712830959953e8174a747b514ca
Josh Noel <joshnoel@mit.edu>
2016-08-13 23:37:00 -0400

    Set mode in LedOn10.py

--- LedOn10.py
+++ LedOn10.py
@@ -6,7 +6,8 @@ import sys
 def main(gpios):
     print("Turning on LED for 10 seconds")
     pins = [int(s) for s in gpios]
-    GPIO.setup(pins, GPIO.OUT, initial = GPIO.HIGH)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pins, GPIO.OUT, initial=GPIO.HIGH)
     time.sleep(10)
     GPIO.output(pins, GPIO.LOW)
     GPIO.cleanup()
